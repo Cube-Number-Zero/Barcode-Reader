a10b4edf346814fbc1d4257a6c6d3dba3840c731
Ryken Copas <143037825+CopasR@users.noreply.github.com>
2026-03-31 10:23:55 -0400

    Update barcodereader.py
    changed cv to cv2

--- barcodereader.py
+++ barcodereader.py
@@ -1,10 +1,10 @@
-import cv2 as cv
+import cv2
 import numpy as np
-img = cv.imread("barcode test.jpg", cv.IMREAD_GRAYSCALE)
+img = cv2.imread("barcode test.jpg", cv2.IMREAD_GRAYSCALE)
 assert img is not None, "file could not be read"
 
-threshold, filtered_img = cv.threshold(cv.GaussianBlur(img,(5,5),0),0,255,\
-                                       cv.THRESH_BINARY+cv.THRESH_OTSU)
+threshold, filtered_img = cv2.threshold(cv2.GaussianBlur(img,(5,5),0),0,255,\
+                                       cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
 print(filtered_img.shape)
-cv.imshow("Display window", cv.resize(filtered_img, (filtered_img.shape[1]//2, filtered_img.shape[0]//2)))
+cv2.imshow("Display window", cv2.resize(filtered_img, (filtered_img.shape[1]//2, filtered_img.shape[0]//2)))

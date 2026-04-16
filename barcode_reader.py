504f1228cceb6811d7d2d99907231ca759b85c5c
Ryken Copas <143037825+CopasR@users.noreply.github.com>
2026-04-16 17:24:27 -0400

    usability changes to scan_line, other misc
    Added a line of code to draw a line where the scan_line function is reading
    Added docstring for scan_line function
    I tried out some test cases with "perfect" barcode images that had nothing but the barcode. It turns out, when our program is given nothing but a barcode, the finder overdoes it and erases the barcode entirely. Catastrophic! Needs fix soon.

--- barcode_reader.py
+++ barcode_reader.py
@@ -2,34 +2,41 @@ import barcode_finder as finder
 import numpy as np
 import cv2
 
-
-def scan_line(image, start_pos_y, angle=0):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def scan_line(image, start_pos_y=None, angle=0):
+    """
+    param image: a binarized cv2 image object
+    param start_pos_y: the y pos to start the scan from. Default is none, which
+                       starts it halfway up the image.
+    param angle: the angle in degrees the scan should be at, default 0 is level
+    returns: list of ints 0 or 1 corresponding to pixel values from the scanned
+             line. 0 for white, 1 for black
+    """
+    if start_pos_y is None:
+        start_pos_y = image.shape[0] // 2
     t = np.tan(np.radians(-angle))
-    winname = "Barcode"
     y = start_pos_y
     x = 0
 
     return_array = []
     
     while x < image.shape[1]:
-        return_array.append(image[y, x] // 255)
+        return_array.append(1-int(image[y, x] // 255))
         x += 1
         y = round(start_pos_y + x * t)
 
+    #cv2.imshow("", cv2.line(image, (0, start_pos_y), (x, y), 5))
     return return_array
     
 
 def read_barcode(path_to_image):
+    # get cropped barcode image from barcode_finder
     image = finder.find_barcode(cv2.imread(path_to_image))
     filtered = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image = filtered
-    print(image)
-    cv2.imshow("Barcode", image)
-
-    
+    _, filtered = cv2.threshold(filtered, 127, 255, cv2.THRESH_BINARY)
+    cv2.imshow("Barcode", filtered)
+    print(scan_line(filtered))
     return NotImplemented
 
 if __name__ == "__main__":
-    path_to_image = "barcode can.jpg"
+    path_to_image = "barcode screenshot.png"
     read_barcode(path_to_image)

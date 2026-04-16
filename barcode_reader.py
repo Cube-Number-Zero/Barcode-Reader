8f24bcd6db7ae861f4eebad4ad804c28b55faf9e
Sophie <cubenumberzero@gmail.com>
2026-04-16 11:22:54 -0400

    added scanning line

--- barcode_reader.py
+++ barcode_reader.py
@@ -1,9 +1,27 @@
 import barcode_finder as finder
+import numpy as np
 import cv2
 
 
+def scan_line(image, start_pos_y, angle=0):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    t = np.tan(np.radians(-angle))
+    winname = "Barcode"
+    y = start_pos_y
+    x = 0
+
+    return_array = []
+    
+    while x < image.shape[1]:
+        return_array.append(image[y, x] // 255)
+        x += 1
+        y = round(start_pos_y + x * t)
+
+    return return_array
+    
+
 def read_barcode(path_to_image):
-    image = finder.find_barcode(cv2.imread(path_to_image), True)
+    image = finder.find_barcode(cv2.imread(path_to_image))
     filtered = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = filtered
     print(image)
